509afe3c7acab33cd6c8df0812c41466d22b9bce
nirch <nirh2@yahoo.com>
2026-02-08 10:17:22 +0200

    comedyOnly state

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -2,14 +2,22 @@
 import Movie from '../components/Movie'
 import './MoviesPage.css'
 import jsonMovies from '../data/movies.json'
+import { useState } from 'react';
 
 export default function MoviesPage() {
-  const movies = jsonMovies;
+  const [movies, setMovies] = useState(jsonMovies);
+  const [comedyOnly, setComedyOnly] = useState(false);
+
+  let displayMovies = movies;
+  if (comedyOnly) {
+    displayMovies = movies.filter(movie => movie.genres.includes('Comedy'))
+  }
 
   return (
     <div className='movies-page'>
       <h1>Movies Page</h1>
-      {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      <button onClick={() => setComedyOnly(!comedyOnly)}>{comedyOnly ? "Show All" : "Comedy Only"}</button>
+      {displayMovies.map(movie => <Movie key={movie.id} movie={movie} />)}
     </div>
   )
 }
