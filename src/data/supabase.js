47ab9f4ab987e283e601502f33adda8995ebad97
nirch <nirh2@yahoo.com>
2026-02-16 11:44:18 +0200

    initializing supabse

---
+++ src/data/supabase.js
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY)
