e70992628dd770b666efb7ee712312207d96d6d5
nirch <nirh2@yahoo.com>
2026-02-16 08:23:54 +0200

    react router + HomePage (using mantine hero header)

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import MoviesPage from './pages/MoviesPage'
 import ActorsPage from './pages/ActorsPage'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
     <>
-      <ActorsPage />
-      <MoviesPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/actors" element={<ActorsPage />} />
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core'
+import { BrowserRouter } from 'react-router';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <MantineProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </MantineProvider>
   </StrictMode>,
 )
